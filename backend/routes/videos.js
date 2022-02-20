deb866c96cae19f63460fada90d68f74e4cce81f
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2022-02-20 19:16:09 +0530

    fixed pagination logic

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -6,7 +6,7 @@ const Video = require('../models/video');
 router.get('/', async (req, res, next) => {
   const page = req.query.page ? req.query.page : 0;
   const page_size = req.query.page_size ? req.query.page_size : 5;
-  const videos = await Video.find({}).sort('-publishedAt').skip(page*page_size).limit(page_size);
+  const videos = await Video.find({}).sort({publishedAt: -1, _id: 1 }).skip(page*page_size).limit(page_size);
 
   res.json(await videos.map(v => v.toJSON()));
 });
