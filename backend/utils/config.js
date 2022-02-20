d542f9276d47453d5ed639a86b7fccdcc9d2b70b
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2022-02-20 17:52:35 +0530

    Updated config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,17 +1,17 @@
-
 const {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   DB_PORT,
   DB_NAME,
+  YOUTUBE_API_KEY,
 } = process.env;
 
 
-const { MONGODB_URI, YOUTUBE_API_KEY } = process.env;
-
+const {DB_URI =  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`} = process.env;
 
 module.exports = {
   YOUTUBE_API_KEY,
-  MONGODB_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-}
+  MONGODB_URI: DB_URI,
+};
+
